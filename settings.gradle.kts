291e2d983b51e704c95a87a88230225cc09834aa
Adam <adam@bootstrapmusic.com>
2023-12-01 11:27:51 -0800

    First commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Alt_13_Android_Wallet"
+include(":app")
+ 
