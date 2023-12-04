65413e6af816b0b64e5b38bff592f1c3a7b8a278
Adam <adam@bootstrapmusic.com>
2023-12-04 15:36:25 -0800

    Added initilization and menu

--- app/src/main/java/com/example/alt_13_android_wallet/InitializeActivity.java
+++ app/src/main/java/com/example/alt_13_android_wallet/InitializeActivity.java
@@ -2,13 +2,38 @@ package com.example.alt_13_android_wallet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class InitializeActivity extends AppCompatActivity {
+
+    EditText editTextNewAccountEmail;
+    Button newAccountButton;
+    Button importAccountButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_initialize);
+
+        editTextNewAccountEmail = findViewById(R.id.editTextNewAccountEmail);
+        newAccountButton = findViewById(R.id.buttonNewAccount);
+        importAccountButton = findViewById(R.id.buttonImportAccount);
+
+        newAccountButton.setOnClickListener(this::initializeNewAccount);
     }
+
+    private void initializeNewAccount(View view) {
+        String email = editTextNewAccountEmail.getText().toString();
+        SharedPreferences sharedPreferences = getSharedPreferences(MainActivity.SHAREDPREFS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(MainActivity.EMAIL_KEY, email);
+        editor.apply();
+        editor.commit();
+        finish();
+    }
+
 }
--- app/src/main/java/com/example/alt_13_android_wallet/MainActivity.java
+++ app/src/main/java/com/example/alt_13_android_wallet/MainActivity.java
@@ -2,13 +2,71 @@ package com.example.alt_13_android_wallet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String SHAREDPREFS = "ANDROIDCLASS";
+    public static final String EMAIL_KEY = "email";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //For testing only
+//        SharedPreferences sharedPreferencesTest = getSharedPreferences(SHAREDPREFS, MODE_PRIVATE);      //Testing only
+//        SharedPreferences.Editor editor = sharedPreferencesTest.edit();
+//        editor.clear().apply();
+
+
+        SharedPreferences sharedPreferences = getSharedPreferences(SHAREDPREFS, MODE_PRIVATE);
+        String email = sharedPreferences.getString(EMAIL_KEY, null);
+        if(email == null){
+            Intent intent = new Intent(getApplicationContext(), InitializeActivity.class);
+            //if needed to pass values in the bundle
+            //otherwise not used
+            Bundle bundle = new Bundle();
+            startActivity(intent, bundle);
+        }else{
+            Log.v("MyTag", email);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SharedPreferences sharedPreferences = getSharedPreferences(SHAREDPREFS, MODE_PRIVATE);
+        String email = sharedPreferences.getString(EMAIL_KEY, null);
+        if(email == null){
+            Log.v("MyTag", "Email is null.");
+            Intent intent = new Intent(getApplicationContext(), InitializeActivity.class);
+            //if needed to pass values in the bundle
+            //otherwise not used
+            Bundle bundle = new Bundle();
+            startActivity(intent, bundle);
+        }else{
+            Log.v("MyTag", email);
+        }
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        int itemID = item.getItemId();
+        if(itemID == R.id.menuItemSettings){
+            Toast.makeText(getApplicationContext(), "Settings button is clicked", Toast.LENGTH_LONG).show();
+        }else if(itemID == R.id.menuItemHistory){
+            Toast.makeText(getApplicationContext(), "History button is clicked", Toast.LENGTH_LONG).show();
+        }
+        return true;
     }
 }
